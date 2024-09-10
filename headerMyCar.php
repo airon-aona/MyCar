3a5591caac984e79d93577d05c668b70b950fa44
Airon <102803348+airon-aona@users.noreply.github.com>
2024-09-10 10:54:01 -0300

    Create headerMyCar.php

---
+++ headerMyCar.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once './connectMyCar.php'; 
+$today = date("Y-m-d");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="../../img/Icon/AzulClaro/IconClaro.png" type="image/x-icon">
+    <title>MyCar</title>
+</head>
+<body>
+
+    <nav>
+        <li><a href="./">Home</a></li>
+        <li><a href="./calculadoraConsumo.php">Consumo</a></li>
+        <li><a href="./veiculos.php">Veículos</a></li>
+    </nav>
+
+</body>
+</html>
